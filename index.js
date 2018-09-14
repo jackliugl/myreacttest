cdcbcebc43da94f8a27f2012b31180127df384ad
LGL-PC\jack <aa@bbd.com>
2018-09-14 00:27:48 -0400

    test4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 const logSomething = options => ({
     ...options,
-    anotherOption: 'Hello!',
+    anotherOption: 'Hello Jack!',
 });
 
 const options = logSomething({ one: '1', two: '2' });
